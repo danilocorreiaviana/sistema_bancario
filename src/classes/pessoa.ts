f48e26d0e6f8a6c8f92272559a7c857ae794abc3
Danilo Correia <danilocorreiaviana@hotmail.com>
2023-07-13 14:41:47 -0300

    Remoção de máscaras de cpf e telefone

--- src/classes/pessoa.ts
+++ src/classes/pessoa.ts
@@ -1,22 +1,20 @@
 abstract class Pessoa {
-    //Criação dos atributos privados
     private cpf: string;
     private nome: string;
     private telefone: string;
 
     constructor(cpf: string, nome: string, telefone: string) {
-        this.cpf = this.formatarCPF(cpf);
+        this.cpf = cpf;
         this.nome = nome;
-        this.telefone = this.formatarTelefone(telefone);
+        this.telefone = telefone;
     }
 
-    //Criação de métodos para obter e setar cada atributo separadamente
     public getCpf(): string {
-        return this.formatarCPF(this.cpf);
+        return this.cpf;
     }
 
     public setCpf(cpf: string): void {
-        this.cpf = this.formatarCPF(cpf);
+        this.cpf = cpf;
     }
 
     public getNome(): string {
@@ -28,19 +26,11 @@ abstract class Pessoa {
     }
 
     public getTelefone(): string {
-        return this.formatarTelefone(this.telefone);
+        return this.telefone;
     }
 
     public setTelefone(telefone: string): void {
-        this.telefone = this.formatarTelefone(telefone);
-    }
-
-    private formatarCPF(cpf: string): string {
-        return cpf.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/, '$1.$2.$3-$4');
-    }
-
-    private formatarTelefone(telefone: string): string {
-        return telefone.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3');
+        this.telefone = telefone;
     }
 
 }
--- src/objects/app1.ts
+++ src/objects/app1.ts
@@ -1,9 +1,9 @@
 import Funcionario from "../classes/funcionario";
 import Cargo from "../classes/cargo";
 
-const funcionario1 = new Funcionario('12345678900', 'Joaquim', '88988888888', 62833.00,);
+const funcionario1 = new Funcionario('123.456.789-00', 'Joaquim', '(88)98888-8888', 62833.00,);
 const cargoGerente = new Cargo('Gerente');
-const funcionario2 = new Funcionario('09876543211', 'Francisco', '88999999999', 1418.00);
+const funcionario2 = new Funcionario('098.765.432-11', 'Francisco', '(88)99999-9999', 1418.00);
 const cargoAtendente = new Cargo('Atendente');
 console.log("--------------------------------------------")
 console.log("APP1 - CRIAÇÃO DE DOIS FUNCIONÁRIOS DO BANCO")
